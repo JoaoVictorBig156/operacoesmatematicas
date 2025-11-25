ee796e7a09072a1cf3034a46b03af827f9883903
JoaoVictorBig156 <joaovirao@gmail.com>
2025-11-25 14:30:09 -0300

    Update multiplicar.c

--- multiplicar.c
+++ multiplicar.c
@@ -1,7 +1,7 @@
 int multiplicar (int numero1,int numero2){
-    contador1 = 0;
-    contador2 = 0;
-    produto = 0;
+    int contador1 = 0;
+    int contador2 = 0;
+    int produto = 0;
 
     while (contador1 < numero1){
         contador2 = 0;
@@ -13,6 +13,7 @@ int multiplicar (int numero1,int numero2){
     }
     return produto;    
 }
+
 
 
 
